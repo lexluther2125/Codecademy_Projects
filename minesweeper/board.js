2742a44a3d205a1c8059d3485a1810bfbbd98131
Lexi <you@example.com>
2017-10-31 20:00:39 -0500

    adding files and components set up to repo

---
+++ minesweeper/board.js
@@ -0,0 +1,90 @@
+export class Board extends Game {
+	constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+		this._numberOfBombs = numberOfBombs;
+		this._numberOfTiles = numberOfRows * numberOfColumns;
+		this._playerBoard = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
+		this._bombBoard = Board.generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs);
+	}
+
+	get playerBoard() {
+		return this._playerBoard;
+	}
+
+	flipTile(rowIndex, columnIndex) {
+	if (this._playerBoard[rowIndex] [columnIndex] !== ' ') {
+		console.log("This tile has already been flipped!");
+		return; 
+	} else if (this._bombBoard[rowIndex] [columnIndex] === 'B') {
+		this._playerBoard[rowIndex] [columnIndex] = 'B';
+	} else {
+		this._playerBoard[rowIndex] [columnIndex] = this.getNumberOfNeighborBombs(rowIndex, columnIndex);
+	}
+	this._numberOfTiles--;
+};
+
+ getNumberOfNeighborBombs(rowIndex, columnIndex) {
+	const neighborOffsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+	const numberOfRows = this._bombBoard.length;
+	const numberOfColumns = this._bombBoard[0].length;
+	let numberOfBombs = 0;
+
+	neighborOffsets.forEach(offset => {
+		const neighborRowIndex = rowIndex + offset[0];
+		const neighborColumnIndex = columnIndex + offset[1];
+
+		if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns) {
+	
+		if (this._bombBoard[neighborRowIndex] [neighborColumnIndex] === 'B') {
+			this._numberOfBombs++;
+		}
+	return this._numberOfBombs;
+}
+});
+};
+
+hasSafeTiles() {
+	return this._numberOfTiles !== this._numberOfBombs;
+};
+
+ print() {
+	console.log(this._playerBoard.map(row => row.join(' | ')).join('\n'));
+}
+
+static generatePlayerBoard(numberOfRows, numberOfColumns) {
+	let board = [];
+
+	for (let numberOfRowsIndex = 0; numberOfRowsIndex < numberOfRows; numberOfRowsIndex++) {
+		let row = [];
+		for (let numberOfColumnsIndex = 0; numberOfColumnsIndex < numberOfColumns; numberOfColumnsIndex++) {
+			row.push(' ');
+		}
+		board.push(row);
+	}
+	return this._playerBoard;
+};
+
+static generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs) {
+		let board = [];
+
+	for (let numberOfRowsIndex = 0; numberOfRowsIndex < numberOfRows; numberOfRowsIndex++) {
+		let row = [];
+		for (let numberOfColumnsIndex = 0; numberOfColumnsIndex < numberOfColumns; numberOfColumnsIndex++) {
+			row.push(null);
+		}
+		board.push(row);
+	}
+	let numberOfBombsPlaced = 0;
+	while (numberOfBombsPlaced < numberOfBombs) {
+
+		let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+		if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+			board[randomRowIndex][randomColumnIndex] = 'B';
+			numberOfBombsPlaced++;
+		}
+	}
+	return this.bombBoard;
+};
+
+};
---
+++ minesweeper/lib/minesweeper.js
@@ -0,0 +1,17 @@
+
+
+// let playerBoard = Board.generatePlayerBoard(3, 4);
+// let bombBoard = Board.generateBombBoard(3, 4, 5);
+
+// console.log("Player Board: ");
+// print(playerBoard);
+// console.log("Bomb Board: ");
+// print(this._bombBoard);
+
+// flipTile(playerBoard, bombBoard, 0, 0);
+// console.log("Updated Player Board: ");
+// print(this._playerBoard);
+
+// const g = new Game(3, 3, 3);
+// playMove(0, 0);
+"use strict";
